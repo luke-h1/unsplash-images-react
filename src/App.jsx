97bf0b18e8cb67e76d1b398762b807b4c49ea7d7
luke-h1 <luke.howsam@yahoo.com>
2020-11-16 19:37:10 +0000

    add image styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.scss';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
+import Home from './components/Pages/Home/Home';
 function App() {
   return (
     <BrowserRouter>
       <>
-        <Navbar />
-        <Switch></Switch>
+        <Switch>
+          <Home />
+        </Switch>
       </>
     </BrowserRouter>
   );
